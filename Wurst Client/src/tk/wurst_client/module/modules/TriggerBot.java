0c0fe250da2fbf028c6925a001c902b46cd2c297
xybytz <95513381+xybytz@users.noreply.github.com>
2024-08-07 16:26:07 -0700

    Update TriggerBot.java

--- Wurst Client/src/tk/wurst_client/module/modules/TriggerBot.java
+++ Wurst Client/src/tk/wurst_client/module/modules/TriggerBot.java
@@ -1,61 +1,207 @@
 /*
- * Copyright © 2014 - 2015 | Alexander01998 | All rights reserved.
+ * Copyright (c) 2014-2024 Wurst-Imperium and contributors.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
  */
-package tk.wurst_client.module.modules;
+package net.wurstclient.hacks;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.EntityLivingBase;
-import tk.wurst_client.Client;
-import tk.wurst_client.module.Category;
-import tk.wurst_client.module.Module;
-import tk.wurst_client.utils.EntityUtils;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
-public class TriggerBot extends Module
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Vec3d;
+import net.wurstclient.Category;
+import net.wurstclient.events.MouseUpdateListener;
+import net.wurstclient.events.UpdateListener;
+import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.AimAtSetting;
+import net.wurstclient.settings.CheckboxSetting;
+import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.settings.SliderSetting.ValueDisplay;
+import net.wurstclient.settings.filterlists.EntityFilterList;
+import net.wurstclient.settings.filters.*;
+import net.wurstclient.util.BlockUtils;
+import net.wurstclient.util.EntityUtils;
+import net.wurstclient.util.Rotation;
+import net.wurstclient.util.RotationUtils;
+
+public final class AimAssistHack extends Hack
+	implements UpdateListener, MouseUpdateListener
 {
-	public TriggerBot()
+	private final SliderSetting range =
+		new SliderSetting("Range", 4.5, 1, 6, 0.05, ValueDisplay.DECIMAL);
+	
+	private final SliderSetting rotationSpeed =
+		new SliderSetting("Rotation Speed", 600, 10, 3600, 10,
+			ValueDisplay.DEGREES.withSuffix("/s"));
+	
+	private final SliderSetting fov =
+		new SliderSetting("FOV", "description.wurst.setting.aimassist.fov", 120,
+			30, 360, 10, ValueDisplay.DEGREES);
+	
+	private final AimAtSetting aimAt = new AimAtSetting(
+		"What point in the target's hitbox AimAssist should aim at.");
+	
+	private final CheckboxSetting checkLOS =
+		new CheckboxSetting("Check line of sight",
+			"description.wurst.setting.aimassist.check_line_of_sight", true);
+	
+	private final CheckboxSetting aimWhileBlocking =
+		new CheckboxSetting("Aim while blocking",
+			"description.wurst.setting.aimassist.aim_while_blocking", false);
+	
+	private final EntityFilterList entityFilters =
+		new EntityFilterList(FilterPlayersSetting.genericCombat(false),
+			FilterSleepingSetting.genericCombat(false),
+			FilterFlyingSetting.genericCombat(0),
+			FilterHostileSetting.genericCombat(false),
+			FilterNeutralSetting
+				.genericCombat(AttackDetectingEntityFilter.Mode.OFF),
+			FilterPassiveSetting.genericCombat(true),
+			FilterPassiveWaterSetting.genericCombat(true),
+			FilterBabiesSetting.genericCombat(true),
+			FilterBatsSetting.genericCombat(true),
+			FilterSlimesSetting.genericCombat(true),
+			FilterPetsSetting.genericCombat(true),
+			FilterVillagersSetting.genericCombat(true),
+			FilterZombieVillagersSetting.genericCombat(true),
+			FilterGolemsSetting.genericCombat(false),
+			FilterPiglinsSetting
+				.genericCombat(AttackDetectingEntityFilter.Mode.OFF),
+			FilterZombiePiglinsSetting
+				.genericCombat(AttackDetectingEntityFilter.Mode.OFF),
+			FilterEndermenSetting
+				.genericCombat(AttackDetectingEntityFilter.Mode.OFF),
+			FilterShulkersSetting.genericCombat(false),
+			FilterInvisibleSetting.genericCombat(true),
+			FilterNamedSetting.genericCombat(false),
+			FilterShulkerBulletSetting.genericCombat(false),
+			FilterArmorStandsSetting.genericCombat(true),
+			FilterCrystalsSetting.genericCombat(true));
+	
+	private Entity target;
+	private float nextYaw;
+	private float nextPitch;
+	
+	public AimAssistHack()
 	{
-		super(
-			"TriggerBot",
-			"Automatically attacks the entity you're looking at.",
-			0,
-			Category.COMBAT);
+		super("AimAssist");
+		setCategory(Category.COMBAT);
+		
+		addSetting(range);
+		addSetting(rotationSpeed);
+		addSetting(fov);
+		addSetting(aimAt);
+		addSetting(checkLOS);
+		addSetting(aimWhileBlocking);
+		
+		entityFilters.forEach(this::addSetting);
 	}
-
+	
 	@Override
-	public void onEnable()
+	protected void onEnable()
 	{
-		if(Client.Wurst.moduleManager.getModuleFromClass(Killaura.class).getToggled())
-			Client.Wurst.moduleManager.getModuleFromClass(Killaura.class).setToggled(false);
-		if(Client.Wurst.moduleManager.getModuleFromClass(KillauraLegit.class).getToggled())
-			Client.Wurst.moduleManager.getModuleFromClass(KillauraLegit.class).setToggled(false);
-		if(Client.Wurst.moduleManager.getModuleFromClass(MultiAura.class).getToggled())
-			Client.Wurst.moduleManager.getModuleFromClass(MultiAura.class).setToggled(false);
+		// disable incompatible hacks
+		WURST.getHax().autoFishHack.setEnabled(false);
+		WURST.getHax().clickAuraHack.setEnabled(false);
+		WURST.getHax().crystalAuraHack.setEnabled(false);
+		WURST.getHax().fightBotHack.setEnabled(false);
+		WURST.getHax().killauraHack.setEnabled(false);
+		WURST.getHax().killauraLegitHack.setEnabled(false);
+		WURST.getHax().multiAuraHack.setEnabled(false);
+		WURST.getHax().protectHack.setEnabled(false);
+		WURST.getHax().tpAuraHack.setEnabled(false);
+		
+		EVENTS.add(UpdateListener.class, this);
+		EVENTS.add(MouseUpdateListener.class, this);
 	}
-
+	
+	@Override
+	protected void onDisable()
+	{
+		EVENTS.remove(UpdateListener.class, this);
+		EVENTS.remove(MouseUpdateListener.class, this);
+		target = null;
+	}
+	
 	@Override
 	public void onUpdate()
 	{
-		if(getToggled()
-			&& Minecraft.getMinecraft().objectMouseOver != null
-			&& Minecraft.getMinecraft().objectMouseOver.entityHit != null)
+		target = null;
+		
+		// don't aim when a container/inventory screen is open
+		if(MC.currentScreen instanceof HandledScreen)
+			return;
+		
+		if(!aimWhileBlocking.isChecked() && MC.player.isUsingItem())
+			return;
+		
+		chooseTarget();
+		if(target == null)
+			return;
+		
+		Vec3d hitVec = aimAt.getAimPoint(target);
+		if(checkLOS.isChecked() && !BlockUtils.hasLineOfSight(hitVec))
 		{
-			updateMS();
-			if(hasTimePassedS(Killaura.realSpeed))
-			{
-				EntityLivingBase en = (EntityLivingBase)Minecraft.getMinecraft().objectMouseOver.entityHit;
-				if(Minecraft.getMinecraft().thePlayer.getDistanceToEntity(en) <= Killaura.realRange
-					&& EntityUtils.isCorrectEntity(en, true))
-				{
-					Criticals.doCritical();
-					Minecraft.getMinecraft().thePlayer.swingItem();
-					Minecraft.getMinecraft().playerController.attackEntity(Minecraft.getMinecraft().thePlayer, en);
-					updateLastMS();
-				}
-			}
+			target = null;
+			return;
 		}
+		
+		WURST.getHax().autoSwordHack.setSlot(target);
+		
+		// get needed rotation
+		Rotation needed = RotationUtils.getNeededRotations(hitVec);
+		
+		// turn towards center of boundingBox
+		Rotation next = RotationUtils.slowlyTurnTowards(needed,
+			rotationSpeed.getValueI() / 20F);
+		nextYaw = next.yaw();
+		nextPitch = next.pitch();
+	}
+	
+	private void chooseTarget()
+	{
+		Stream<Entity> stream = EntityUtils.getAttackableEntities();
+		
+		double rangeSq = range.getValueSq();
+		stream = stream.filter(e -> MC.player.squaredDistanceTo(e) <= rangeSq);
+		
+		if(fov.getValue() < 360.0)
+			stream = stream.filter(e -> RotationUtils.getAngleToLookVec(
+				aimAt.getAimPoint(e)) <= fov.getValue() / 2.0);
+		
+		stream = entityFilters.applyTo(stream);
+		
+		target = stream
+			.min(Comparator.comparingDouble(
+				e -> RotationUtils.getAngleToLookVec(aimAt.getAimPoint(e))))
+			.orElse(null);
+	}
+	
+	@Override
+	public void onMouseUpdate(MouseUpdateEvent event)
+	{
+		if(target == null || MC.player == null)
+			return;
+		
+		float curYaw = MC.player.getYaw();
+		float curPitch = MC.player.getPitch();
+		int diffYaw = (int)(nextYaw - curYaw);
+		int diffPitch = (int)(nextPitch - curPitch);
+		
+		// If we are <1 degree off but still missing the hitbox,
+		// slightly exaggerate the difference to fix that.
+		if(diffYaw == 0 && diffPitch == 0 && !RotationUtils
+			.isFacingBox(target.getBoundingBox(), range.getValue()))
+		{
+			diffYaw = nextYaw < curYaw ? -1 : 1;
+			diffPitch = nextPitch < curPitch ? -1 : 1;
+		}
+		
+		event.setDeltaX(event.getDefaultDeltaX() + diffYaw);
+		event.setDeltaY(event.getDefaultDeltaY() + diffPitch);
 	}
 }
